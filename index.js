a0819bd5e1d40b60d95216f23d745d0a88196094
mararo-ops <danielmararo1@gmail.com>
2023-10-10 16:02:31 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
   //add static methods here
 }
+class Formatter {
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^a-zA-Z0-9-\s']/g, '');
+  }
+
+  static titleize(string) {
+    const words = string.split(' ');
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0 || !exceptions.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    });
+
+    return titleizedWords.join(' ');
+  }
+}
